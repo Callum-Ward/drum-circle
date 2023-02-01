49af2c004036ba1c2cb02f1d73307cb04d6de39e
Wardy004 <67753830+Wardy004@users.noreply.github.com>
2023-02-01 20:54:10 +0000

    object moving/deleting and spawning scripts added

---
+++ Drum-Circle/Assets/Scripts/MoveBeat.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBeat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 5;
+    public float removeHeight = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        if (transform.position.y < removeHeight)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
