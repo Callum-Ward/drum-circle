49658ebdd20e92d3615c51f4a1965bfcd5d0a4ba
Wardy004 <67753830+Wardy004@users.noreply.github.com>
2023-02-02 09:40:06 +0000

    beat spawning and logic seperated into 2 scripts, added zones for 1,2,3 player with each player having 2 beats to play

---
+++ Drum-Circle/Assets/RhythmScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RhythmScript : MonoBehaviour
+{
+    public RhythmSpawner spawner;
+    private float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawner = GameObject.FindGameObjectWithTag("Spawner").GetComponent<RhythmSpawner>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 2)
+        {
+            spawner.spawn(1, 1);
+            spawner.spawn(1, 0);
+            spawner.spawn(3, 1);
+            spawner.spawn(3, 0);
+            spawner.spawn(5, 1);
+            spawner.spawn(5, 0);
+            timer = 0;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+        
+        
+    }
+}
