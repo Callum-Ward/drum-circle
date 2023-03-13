71301c232edc5dc3ec91f4f01d12a320b83f1a3e
Wardy004 <callumward2000@gmail.com>
2023-03-13 15:16:34 +0000

    Arduino script forwards hit strength

--- Arduino/Impulse_plot/Impulse_plot.ino
+++ Arduino/Impulse_plot/Impulse_plot.ino
@@ -6,10 +6,21 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   int val1 = analogRead(0);
-  int val2 = analogRead(2);
-  int val3 = analogRead(3);  
+  int val2 = analogRead(1);
+  int val3 = analogRead(2);  
+  int val4 = analogRead(3);  
   
-  if (val1 > 500) {
+  if (val1 > 200) {
     Serial.println(val1);
   }
+  if (val2 > 200) {
+    Serial.println(val2);
+  }
+  if (val3 > 200) {
+    Serial.println(val3);
+  }
+    if (val4 > 200) {
+    Serial.println(val4);
+  }
+
 }
