2d78bd6e27a331f9926e5e2b88adace0b54a318b
AngusJPurcell <purciea242@aol.co.uk>
2023-04-18 15:39:53 +0100

    3 person tracking to model rigging

--- Drum-Circle/Assets/TrackerScripts/PuppetAvatar.cs
+++ Drum-Circle/Assets/TrackerScripts/PuppetAvatar.cs
@@ -7,12 +7,21 @@ using System.Text;
 public class PuppetAvatar : MonoBehaviour
 {
     public TrackerHandler KinectDevice;
-    Dictionary<JointId, Quaternion> absoluteOffsetMap;
-    Animator PuppetAnimator;
-    public GameObject RootPosition;
-    public Transform CharacterRootTransform;
-    public float OffsetY;
-    public float OffsetZ;
+    Dictionary<JointId, Quaternion> absoluteOffsetMap1;
+    Dictionary<JointId, Quaternion> absoluteOffsetMap2;
+    Dictionary<JointId, Quaternion> absoluteOffsetMap3;
+    Animator PuppetAnimator1;
+    Animator PuppetAnimator2;
+    Animator PuppetAnimator3;
+    public Transform CharacterRootTransform1;
+    public Transform CharacterRootTransform2;
+    public Transform CharacterRootTransform3;
+    public float OffsetY1;
+    public float OffsetZ1;
+    public float OffsetY2;
+    public float OffsetZ2;
+    public float OffsetY3;
+    public float OffsetZ3;
     private static HumanBodyBones MapKinectJoint(JointId joint)
     {
         // https://docs.microsoft.com/en-us/azure/Kinect-dk/body-joints
@@ -44,26 +53,61 @@ public class PuppetAvatar : MonoBehaviour
     }
     private void Start()
     {
-        PuppetAnimator = GetComponent<Animator>();
-        Transform _rootJointTransform = CharacterRootTransform;
+        PuppetAnimator1 = transform.GetChild(0).GetComponent<Animator>();
+        Transform _rootJointTransform1 = CharacterRootTransform1;
 
-        absoluteOffsetMap = new Dictionary<JointId, Quaternion>();
+        PuppetAnimator2 = transform.GetChild(1).GetComponent<Animator>();
+        Transform _rootJointTransform2 = CharacterRootTransform2;
+       
+        PuppetAnimator3 = transform.GetChild(2).GetComponent<Animator>();
+        Transform _rootJointTransform3 = CharacterRootTransform3;
+
+        absoluteOffsetMap1 = new Dictionary<JointId, Quaternion>();
+        absoluteOffsetMap2 = new Dictionary<JointId, Quaternion>();
+        absoluteOffsetMap3 = new Dictionary<JointId, Quaternion>();
         for (int i = 0; i < (int)JointId.Count; i++)
         {
             HumanBodyBones hbb = MapKinectJoint((JointId)i);
             if (hbb != HumanBodyBones.LastBone)
             {
-                Transform transform = PuppetAnimator.GetBoneTransform(hbb);
-                Quaternion absOffset = GetSkeletonBone(PuppetAnimator, transform.name).rotation;
+                Transform transform = PuppetAnimator1.GetBoneTransform(hbb);
+                Quaternion absOffset = GetSkeletonBone(PuppetAnimator1, transform.name).rotation;
                 // find the absolute offset for the tpose
-                while (!ReferenceEquals(transform, _rootJointTransform))
+                while (!ReferenceEquals(transform, _rootJointTransform1))
                 {
                     transform = transform.parent;
-                    absOffset = GetSkeletonBone(PuppetAnimator, transform.name).rotation * absOffset;
+                    absOffset = GetSkeletonBone(PuppetAnimator1, transform.name).rotation * absOffset;
                 }
-                absoluteOffsetMap[(JointId)i] = absOffset;
+                absoluteOffsetMap1[(JointId)i] = absOffset;
+            }
+            
+            if (hbb != HumanBodyBones.LastBone)
+            {
+                Transform transform = PuppetAnimator2.GetBoneTransform(hbb);
+                Quaternion absOffset = GetSkeletonBone(PuppetAnimator2, transform.name).rotation;
+                // find the absolute offset for the tpose
+                while (!ReferenceEquals(transform, _rootJointTransform2))
+                {
+                    transform = transform.parent;
+                    absOffset = GetSkeletonBone(PuppetAnimator2, transform.name).rotation * absOffset;
+                }
+                absoluteOffsetMap2[(JointId)i] = absOffset;
+            }
+
+            if (hbb != HumanBodyBones.LastBone)
+            {
+                Transform transform = PuppetAnimator3.GetBoneTransform(hbb);
+                Quaternion absOffset = GetSkeletonBone(PuppetAnimator3, transform.name).rotation;
+                // find the absolute offset for the tpose
+                while (!ReferenceEquals(transform, _rootJointTransform3))
+                {
+                    transform = transform.parent;
+                    absOffset = GetSkeletonBone(PuppetAnimator3, transform.name).rotation * absOffset;
+                }
+                absoluteOffsetMap3[(JointId)i] = absOffset;
             }
         }
+
     }
 
     private static SkeletonBone GetSkeletonBone(Animator animator, string boneName)
@@ -87,19 +131,63 @@ public class PuppetAvatar : MonoBehaviour
     {
         for (int j = 0; j < (int)JointId.Count; j++)
         {
-            if (MapKinectJoint((JointId)j) != HumanBodyBones.LastBone && absoluteOffsetMap.ContainsKey((JointId)j))
+            if (MapKinectJoint((JointId)j) != HumanBodyBones.LastBone && absoluteOffsetMap1.ContainsKey((JointId)j))
             {
+
+                // Vector3 RootPosition = new Vector3(0.0f, 0.5f, 0.0f);
+                Vector3 RootPosition = KinectDevice.pelvis1;
                 // get the absolute offset
-                Quaternion absOffset = absoluteOffsetMap[(JointId)j];
-                Transform finalJoint = PuppetAnimator.GetBoneTransform(MapKinectJoint((JointId)j));
-                finalJoint.rotation = absOffset * Quaternion.Inverse(absOffset) * KinectDevice.absoluteJointRotations[j] * absOffset;
+                Quaternion absOffset = absoluteOffsetMap1[(JointId)j];
+                Transform finalJoint = PuppetAnimator1.GetBoneTransform(MapKinectJoint((JointId)j));
+
+                finalJoint.rotation = absOffset * Quaternion.Inverse(absOffset) * KinectDevice.absoluteJointRotations1[j] * absOffset;
+                
                 if (j == 0)
                 {
+                    UnityEngine.Debug.Log("root1: " + RootPosition.x + RootPosition.y);
                     // character root plus translation reading from the kinect, plus the offset from the script public variables
-                    finalJoint.position = CharacterRootTransform.position + new Vector3(RootPosition.transform.localPosition.x, RootPosition.transform.localPosition.y + OffsetY, RootPosition.transform.localPosition.z - OffsetZ);
+                    finalJoint.position = CharacterRootTransform1.position + new Vector3(RootPosition.x, RootPosition.y + OffsetY1, RootPosition.z - OffsetZ1);
+                    // finalJoint.position = CharacterRootTransform1.position + new Vector3(transform.GetChild(0).position.x, transform.GetChild(0).position.y + OffsetY1, transform.GetChild(0).position.z - OffsetZ1);
+                }
+            }
+            if (MapKinectJoint((JointId)j) != HumanBodyBones.LastBone && absoluteOffsetMap2.ContainsKey((JointId)j))
+            {
+                // Vector3 RootPosition = new Vector3(-2.0f, 0.5f, 0.0f);
+                Vector3 RootPosition = KinectDevice.pelvis2;
+                // get the absolute offset
+                Quaternion absOffset = absoluteOffsetMap2[(JointId)j];
+                Transform finalJoint = PuppetAnimator2.GetBoneTransform(MapKinectJoint((JointId)j));
+
+                finalJoint.rotation = absOffset * Quaternion.Inverse(absOffset) * KinectDevice.absoluteJointRotations2[j] * absOffset;
+                
+                if (j == 0)
+                {
+                    UnityEngine.Debug.Log("root2: " + RootPosition.x + RootPosition.y);
+
+                    // character root plus translation reading from the kinect, plus the offset from the script public variables
+                    finalJoint.position = CharacterRootTransform2.position + new Vector3(RootPosition.x, RootPosition.y + OffsetY2, RootPosition.z - OffsetZ2);                    
+                    // finalJoint.position = CharacterRootTransform1.position + new Vector3(transform.GetChild(1).position.x, transform.GetChild(1).position.y + OffsetY2, transform.GetChild(1).position.z - OffsetZ2);
+                }
+            }
+            if (MapKinectJoint((JointId)j) != HumanBodyBones.LastBone && absoluteOffsetMap3.ContainsKey((JointId)j))
+            {
+                // Vector3 RootPosition = new Vector3(-4.0f, 0.5f, 0.0f);
+                Vector3 RootPosition = KinectDevice.pelvis3;
+                // get the absolute offset
+                Quaternion absOffset = absoluteOffsetMap3[(JointId)j];
+                Transform finalJoint = PuppetAnimator3.GetBoneTransform(MapKinectJoint((JointId)j));
+
+                finalJoint.rotation = absOffset * Quaternion.Inverse(absOffset) * KinectDevice.absoluteJointRotations3[j] * absOffset;
+                
+                if (j == 0)
+                {
+                    UnityEngine.Debug.Log("root3: " + RootPosition.x + RootPosition.y);
+
+                    // character root plus translation reading from the kinect, plus the offset from the script public variables
+                    finalJoint.position = CharacterRootTransform3.position + new Vector3(RootPosition.x, RootPosition.y + OffsetY3, RootPosition.z - OffsetZ3);                    
+                    // finalJoint.position = CharacterRootTransform1.position + new Vector3(transform.GetChild(1).position.x, transform.GetChild(1).position.y + OffsetY2, transform.GetChild(1).position.z - OffsetZ2);
                 }
             }
         }
     }
-
 }
