3396e1dec97d02c92870c886079bf963599aa24c
Paul Vladislav <54722788+computerwiz27@users.noreply.github.com>
2023-05-10 18:12:00 +0100

    Commented TreeManager.cs

--- Drum-Circle/Assets/Scripts/Tree Gen/TreeManager.cs
+++ Drum-Circle/Assets/Scripts/Tree Gen/TreeManager.cs
@@ -1,7 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class TreeManager : MonoBehaviour
@@ -11,12 +8,12 @@ public class TreeManager : MonoBehaviour
     [SerializeField] bool testing = false;
 
     ScoreManager scoreManager = null;
-    
-    float lastScore = 0;
+
     bool hitStatus = false;
 
     void Start()
     {
+        //Get all the procedural trees
         trees = GameObject.FindGameObjectsWithTag("Procedural Tree");
         if (!testing)
         {
@@ -26,6 +23,7 @@ public class TreeManager : MonoBehaviour
 
     void Update()
     {
+        //this is used for debugging/development
         if (testing)
         {
             foreach (var t in trees)
@@ -39,6 +37,7 @@ public class TreeManager : MonoBehaviour
 
         else
         {
+            //Get just the trees that are growing
             var growingTrees = new List<GameObject>( GameObject.FindGameObjectsWithTag("Procedural Tree") );
             foreach (var t in growingTrees.ToArray())
             {
@@ -50,13 +49,14 @@ public class TreeManager : MonoBehaviour
             }
             this.trees = growingTrees.ToArray();
 
-            if (hitStatus == true)
+            if (hitStatus == true) //this is always true (legacy)
             {
                 
                 foreach (var t in this.trees)
                 {
                     var tree = t.GetComponent<Tree>();
 
+                    //avrage the score multipliers
                     float scoreMul = 0;
                     foreach (var mul in scoreManager.ScoreMultiplier) {
                         scoreMul += mul;
@@ -64,14 +64,16 @@ public class TreeManager : MonoBehaviour
                     if (scoreMul < 3) scoreMul = 3;
                     scoreMul = scoreMul / 3f;
 
+                    //grow the tree based on the score multiplier average and growth rate
                     tree.Grow(scoreMul * growthRate);
-
+                    //always try to add branches 
                     tree.AddBranches(0);
                 }
             }
         }
     }
 
+    //legacy, please ignore
     public void SetHitStatus(bool hit) 
     {
         hitStatus = hit;
